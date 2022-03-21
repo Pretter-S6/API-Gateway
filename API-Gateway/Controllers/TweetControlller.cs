1b575ff756139984275142c4bb85a1a5d0c8cb35
Rienk Engbrenghof <i429059@fhict.nl>
2022-03-21 15:20:52 +0100

    Controller and models added

---
+++ API-Gateway/Controllers/TweetControlller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gateway.Controllers
+{
+    [ApiController]
+    [Route("/test")]
+    public class TweetController : ControllerBase
+    {
+
+        private readonly ILogger<TweetController> _logger;
+
+        public TweetController(ILogger<TweetController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string test = "test";
+            return test;
+        }
+    }
+}
---
+++ API-Gateway/Models/Tweet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gateway.Models
+{
+    public class Tweet
+    {
+        public Tweet()
+        {
+            reactions = new List<string>();
+        }
+
+        public int tweetID { get; set; }
+        public string text { get; set; }
+        public DateTime uploadTime { get; set; }
+        public List<User> likes { get; set; }
+        public List<string> reactions { get; set; }
+
+
+    }
+}
